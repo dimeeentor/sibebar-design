a264169e88de923ef85e64a5301941a71d053e08
Dmytro Honcharenko <dimeeentor@gmail.com>
2023-04-13 22:29:59 +0200

    Changed logic how menu is rendered

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,39 @@
 import Image from "next/image"
 import Link from "next/link"
+import { Ref, RefObject, useRef } from "react"
 
 export default function Home() {
+    const overview = useRef<HTMLUListElement>(null)
+    const store = useRef<HTMLUListElement>(null)
+    const emails = useRef<HTMLUListElement>(null)
+    const reports = useRef<HTMLUListElement>(null)
+    const design = useRef<HTMLUListElement>(null)
+    const discounts = useRef<HTMLUListElement>(null)
+    const licenses = useRef<HTMLUListElement>(null)
+
+    const menuItems = [
+        "Overview",
+        "Store",
+        "Emails",
+        "Reports",
+        "Design",
+        "Discounts",
+        "Licenses",
+    ]
+
+    const menuItemsRefs = useRef<HTMLUListElement[]>([])
+
+    function doOpenMenu(menuItem: HTMLUListElement) {
+        return menuItem.classList.toggle("hidden")
+    }
+
     return (
-        <aside className="h-screen w-80 rounded-r-lg bg-[#0050F4]/90 p-4">
+        <aside className="flex h-screen w-80 justify-between bg-[#0050F4]/90 p-4">
             <aside className="[&>img]:ease flex h-full w-10 flex-col items-center">
                 {/* Account Photo */}
                 <div className="mb-4 h-10 w-10 flex-none cursor-pointer rounded-full bg-white shadow-md"></div>
                 {/* Sidebar Icons - Top */}
-                <div className="ease flex w-fit grow flex-col items-center gap-2 transition [&>*]:duration-300">
+                <nav className="ease flex w-fit grow flex-col items-center gap-2">
                     <Link href={""} className="default-hover">
                         <Image
                             src={"/../public/icons/home.png"}
@@ -45,9 +70,9 @@ export default function Home() {
                             className="invert"
                         />
                     </Link>
-                </div>
+                </nav>
                 {/* Sidebar Icons - Bottom */}
-                <div className="mb-8 cursor-pointer rounded-lg p-2 duration-300 hover:bg-blue-500">
+                <div className="default-hover mb-8 rounded-lg">
                     <Image
                         src={"/../public/icons/contacts.png"}
                         height={26}
@@ -59,8 +84,8 @@ export default function Home() {
                 {/* Account Photo */}
                 <div className="h-10 w-10 flex-none cursor-pointer rounded-full bg-white shadow-md"></div>
             </aside>
-            <main className="absolute left-20 top-5">
-                <header className="flex max-w-full items-center justify-between [&>*]:duration-300">
+            <main className="overflow-y-scroll">
+                <header className="flex max-w-full items-center justify-between">
                     <h2 className="text-white">sidebardesign.com</h2>
                     <div className="default-hover ml-14 cursor-pointer">
                         <Image
@@ -72,55 +97,36 @@ export default function Home() {
                         />
                     </div>
                 </header>
-                <menu className="mt-6 flex flex-col gap-1 text-white [&>*]:duration-300">
-                    <menu className="flex-col">
-                        <div className="default-hover flex justify-between">
-                            <span>Overview</span>
-                            <span className="text-xl">+</span>
-                        </div>
-                        <ul className="px-2">
-                            <li className="list-disc">Item 1</li>
-                            <li className="list-disc">Item 2</li>
-                            <li className="list-disc">Item 3</li>
-                            <li className="list-disc">Item 4</li>
-                        </ul>
-                    </menu>
-                    <menu className="default-hover">
-                        <div className="flex justify-between">
-                            <span>Store</span>
-                            <span className="text-xl">+</span>
-                        </div>
-                    </menu>
-                    <menu className="default-hover">
-                        <div className="flex justify-between">
-                            <span>Emails</span>
-                            <span className="text-xl">+</span>
-                        </div>
-                    </menu>
-                    <menu className="default-hover">
-                        <div className="flex justify-between">
-                            <span>Reports</span>
-                            <span className="text-xl">+</span>
-                        </div>
-                    </menu>
-                    <menu className="default-hover">
-                        <div className="flex justify-between">
-                            <span>Design</span>
-                            <span className="text-xl">+</span>
-                        </div>
-                    </menu>
-                    <menu className="default-hover">
-                        <div className="flex justify-between">
-                            <span>Discounts</span>
-                            <span className="text-xl">+</span>
-                        </div>
-                    </menu>
-                    <menu className="default-hover">
-                        <div className="flex justify-between">
-                            <span>Licenses</span>
-                            <span className="text-xl">+</span>
-                        </div>
-                    </menu>
+                <menu className="mt-6 flex flex-col gap-1 text-white">
+                    {menuItems.map((element, index) => {
+                        return (
+                            <menu key={element}>
+                                <div
+                                    className="default-hover flex justify-between"
+                                    onClick={(e) => {
+                                        console.log(e.target)
+                                        doOpenMenu(menuItemsRefs.current[index])
+                                    }}
+                                >
+                                    <span>{element}</span>
+                                    <span className="close-menu relative text-xl">
+                                        —
+                                    </span>
+                                </div>
+                                <ul
+                                    ref={(element: HTMLUListElement) =>
+                                        menuItemsRefs.current.push(element)
+                                    }
+                                    className="hidden px-2"
+                                >
+                                    <li className="list-disc">Item 1</li>
+                                    <li className="list-disc">Item 2</li>
+                                    <li className="list-disc">Item 3</li>
+                                    <li className="list-disc">Item 4</li>
+                                </ul>
+                            </menu>
+                        )
+                    })}
                 </menu>
             </main>
         </aside>
